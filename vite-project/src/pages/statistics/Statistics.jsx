aca2f9b0f0e7f3c46ae59905d3f18a5083f10b8f
Rakib mia <rbepari404@gmail.com>
2023-09-26 00:27:01 -0600

    Statistics funcnatilty implement

--- vite-project/src/pages/statistics/Statistics.jsx
+++ vite-project/src/pages/statistics/Statistics.jsx
@@ -1,61 +1,101 @@
 import React, { useEffect, useState } from "react";
 // import { PieChart, ResponsiveContainer, Pie, Cell } from 'recharts';
-import localStor from '../../utils/localStorage'
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
-
+import localStor from "../../utils/localStorage";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
 const Statistics = () => {
-  const lsItems = localStor.getDonation()
-  const [items, setItems] = useState([])
-  
+  const lsItems = localStor.getDonation();
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
-    fetch('data.json')
-    .then((res) => res.json())
-    .then((data) => setItems(data))
-  },[])
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
 
-  console.log((items.length)/100 * lsItems.length);
+  const COLORS = ["#FF444A", "#00C49F"];
 
-  const data = [
-    { value: 1400 },
-    { value: 600 },
-  ]
-
-  const COLORS = ["#0088FE", "#00C49F",];
-
-    const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
-  
+
     return (
-      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
         {`${(percent * 100).toFixed(0)}%`}
       </text>
-      );
-    };
+    );
+  };
+
+  function calculateDonationPercentage() {
+    if (items.length === 0) {
+      return 0;
+    }
+    return (lsItems.length / items.length) * 100;
+  }
+
+  const data = [
+    { value: 100 - calculateDonationPercentage() },
+    { value: calculateDonationPercentage() },
+  ];
+
+  console.log(data);
 
   return (
-    <div className="flex h-96 justify-center items-center">
-         <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
+    <div>
+      <div className="flex h-72 justify-center items-center">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="flex my-6 justify-center">
+        <div className="md:flex gap-11">
+          <div className="flex items-center gap-2">
+            <p>Your Donation</p>
+            <div className="w-10 h-3 rounded-md bg-[#00C49F]"></div>
+          </div>
+
+          <div>
+            <div className="flex items-center gap-2">
+              <p>Total Donation</p>
+              <div className="w-10 h-3 rounded-md bg-[#FF444A]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
